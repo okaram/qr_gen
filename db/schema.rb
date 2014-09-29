f54edd4c855aac3ccf51f153b478e539dda4d0e3
Orlando Karam <orlando.karam@gmail.com>
2014-09-29 01:42:58 +0000

    trying heroku auto again

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,4 @@
 
 ActiveRecord::Schema.define(:version => 20130306031038) do
 
-  create_table "things", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
 end
